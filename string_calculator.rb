402c713c2f62fef4b4acbdb1cf279ed776e04d58
aachal <aachaltayade1997@gmail.com>
2024-02-27 00:28:22 +0530

    Enhance Error Handling and Refactor of StringCalculator#add method

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,25 +1,29 @@
 class StringCalculator
 
   def add(string)
-    return 0 if string.nil?
+    return 0 if string.empty?
 
-    numbers = string.split(/[,\n]/).map(&:to_i)
+    numbers = string.split(/\/\/|[,;\n]/).map(&:to_i)
+
+    handle_negative_numbers(numbers) # to check for negative numbers and raise an exception if any
+    # Return the sum of the numbers
+    numbers.sum
+  end
+
+  private
+
+  def handle_negative_numbers(numbers)
     negatives = numbers.select { |num| num.negative? }
-
-    if negatives.any?
-      raise ArgumentError, "Negatives not allowed: #{negatives.join(',')}"
-    else
-      numbers.sum
-    end
+    # Raise an exception with a message if negative numbers are found
+    raise ArgumentError, "Negatives not allowed: #{negatives.join(',')}" if negatives.any?
   end
 
   calculator = StringCalculator.new
   input_string = "10,\n20\n30,1"
   begin
     result = calculator.add(input_string)
-    p result
+    puts result
   rescue ArgumentError => e
     puts "Error: #{e.message}"
   end
 end
-
